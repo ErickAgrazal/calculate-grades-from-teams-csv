a4a04e53f6cdb1ee634c8d57f5eaccc4c4974622
Erick Agrazal <erick.agrazal@gmail.com>
2021-12-28 11:32:48 -0500

    Updated calculate grades.

--- calculate_grades.py
+++ calculate_grades.py
@@ -1,7 +1,9 @@
+import collections
 import csv
 import os
 
-FILENAME = os.environ.get('FILENAME', 'export.csv')
+IMPORT_FILE = os.environ.get('IMPORT_FILENAME', 'import2.csv')
+EXPORT_FILE = os.environ.get('EXPORT_FILENAME', 'export.csv')
 
 
 def get_float(number):
@@ -12,44 +14,54 @@ def get_float(number):
     return number
 
 
-with open(FILENAME, newline='') as csvfile:
-    exported_file = csv.DictReader(csvfile)
-    for row in exported_file:
-        first_name = row['\ufeff"First Name"']
-        last_name = row['Last Name']
-        email = row['Email Address']
-        semestral = get_float(row['Semestral']) * 0.35
-        parciales = (
-            get_float(row['Parcial #2 - Implementación de aplicación de TO-DO']),
-            get_float(row['Presentación #1 - Documento escrito']),
-            get_float(row['Presentación #1 - Presentación oral'])
-        )
-        laboratorios = (
-            get_float(row['Laboratorio #8 - Continuación de ejemplos de ExpressJS (Docker)']),
-            get_float(row['Laboratorio #7 - Continuación de ejemplos de ExpressJS (Frontend)']),
-            get_float(row['Laboratorio #6 - Continuación de ejemplos de ExpressJS']),
-            get_float(row['Laboratorio #5 - Problemas usando JS con HTML tablas']),
-            get_float(row['Laboratorio #4 - Problemas de aplicación usando JS con HTML']),
-            get_float(row['Laboratorio #3 - Problemas de aplicación usando JS ']),
-            get_float(row['Laboratorio #2 - Problemas básicos con Javascript']),
-            get_float(row['Laboratorio #1 - Crear cuenta de github']),
-        )
-        parciales = (sum(parciales) / len(parciales)) * 0.30
-        print(f'parciales: {parciales}')
-        laboratorios = (sum(laboratorios) / len(laboratorios)) * 0.30
-        print(f'laboratorios: {laboratorios}')
-        print(f'semestral: {semestral}')
-        asistencia = 0.05
-        grade = semestral + parciales + laboratorios + asistencia
-        if grade < 61:
-            grade_in_letter = 'F'
-        elif grade < 71:
-            grade_in_letter = 'D'
-        elif grade < 81:
-            grade_in_letter = 'C'
-        elif grade < 91:
-            grade_in_letter = 'B'
-        else:
-            grade_in_letter = 'A'
-        print(f'{first_name} {last_name}: {grade} ({grade_in_letter})')
-        print()
+with open(IMPORT_FILE, newline='') as csvfile:
+    counter = {}
+    with open(EXPORT_FILE, 'w') as exportfile:
+        writer = csv.writer(exportfile)
+        writer.writerow(('nombre', 'correo', 'parciales', 'laboratorios', 'semestral', 'final', 'final en letra'))
+        exported_file = csv.DictReader(csvfile)
+        for row in exported_file:
+            first_name = row['\ufeff"Nombre"']
+            last_name = row['Apellidos']
+            email = row['Dirección de correo']
+            semestral_raw = row['Examen semestral - Construir CV ']
+            semestral = get_float(semestral_raw) * 0.35
+            parciales = (
+                get_float(row['Parcial # 1']),
+                get_float(row['Proyecto #1 - Implementar el HTML y CSS en base al Wireframe']),
+                get_float(row['Presentación #1 - Trabajo escrito']),
+                get_float(row['Presentación #1 - Presentación Oral'])
+            )
+            laboratorios = (
+                get_float(row['Laboratorio #8 - Construir frontend para calculadora']),
+                get_float(row['Laboratorio #7 - Crear endpoints para calculadora']),
+                get_float(row['Laboratorio #6 - Aplicar Flexbox a formulario de estudiantes']),
+                get_float(row['Laboratorio #5 - Generar cartas usando CSS (Flexbox)']),
+                get_float(row['Laboratorio #4 - Aplicar CSS a un formularios básicos']),
+                get_float(row['Laboratorio #3 - Crear documento HTML con formularios básicos']),
+                get_float(row['Laboratorio #2 - Crear documento HTML con etiquetas de visualización']),
+                get_float(row['Laboratorio #1 - Crear documento HTML con la información sobre la ']),
+            )
+            parciales = (sum(parciales) / len(parciales)) * 0.30
+            laboratorios = (sum(laboratorios) / len(laboratorios)) * 0.30
+            asistencia = 0.05
+            grade = round(semestral + parciales + laboratorios + asistencia)
+            if grade < 61:
+                grade_in_letter = 'F'
+            elif grade < 71:
+                grade_in_letter = 'D'
+            elif grade < 81:
+                grade_in_letter = 'C'
+            elif grade < 91:
+                grade_in_letter = 'B'
+            else:
+                grade_in_letter = 'A'
+            counter[grade_in_letter] = counter.get(grade_in_letter, 0) + 1
+            print(f'{first_name} {last_name}: {grade} ({grade_in_letter})')
+            writer.writerow((f'{first_name} {last_name}', email, parciales, laboratorios, semestral_raw, grade, grade_in_letter))
+        writer.writerow(('-------------------', ))
+        writer.writerow(('letra', 'cantidad'))
+        ordered_dict = collections.OrderedDict(sorted(counter.items()))
+        for letter, count in ordered_dict.items():
+            writer.writerow((letter, count))
+        print('Exported')
