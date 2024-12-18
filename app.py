9e5af4a8059870592a2647c928d06440e4b2693c
Erick Vicente Agrazal Lopez <erick@agrazal.com>
2024-12-18 18:44:41 -0500

    Updated to new standards

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ csv_file = pandas.read_csv('import.csv')
 percentages = {
   'labs': 0.15,
   'parcials': 0.3,
-  'projects': 0.15,
+  'projects': 0.1,
   'final': 0.35,
   'attendance': 0.05,
+  'portfolio': 0.05,
 }
 
 general_headers = [
@@ -25,10 +26,15 @@ lab_headers = [
   'Laboratorio #4 - Formulario sin usar evento submit',
   'Laboratorio #5 - Formulario sin usar evento submit agregando clases',
   'Laboratorio #6 - Flexbox',
+  'Laboratorio #7 - Implementar aplicación para votación',
+  'Laboratorio #8 - Grid',
+  'Laboratorio #9 - Implementar aplicación basada en consumo de PokeApi',
 ]
 
 parcial_headers = [
   'Parcial #1 - Tema #1',
+  'Parcial #2 - Implementación de solución usando HTML/CSS/JS',
+  'Parcial #3 - Implementar aplicación basada en consumo de PokeApi',
 ]
 
 project_headers = [
@@ -40,17 +46,22 @@ attendance_headers = [
   'Asistencia',
 ]
 
+portfolio_headers = [
+  'Portafolio',
+]
+
 final_headers = [
-  'Final',
+  'Semestral',
 ]
 # -------------------------------
 
 # -------------------------------
 #  Special operations
-csv_file['Parcial #1 - Tema #1'] = csv_file['Parcial #1 - Tema #1'] * 2
-csv_file['Asistencia'] = 100
-csv_file['Final'] = 100
+csv_file['Parcial #1 - Tema #1'] = csv_file['Parcial #1 - Tema #1'] * 2 # Double the grade of parcial #1, because the points are based on 50.
+csv_file['Parcial #1 - Tema #1'] = csv_file['Parcial #1 - Tema #1'].fillna(csv_file['Semestral']) # If a parcial #1 is empty, get the grade from the semestral
+csv_file['Asistencia'] = 100 # Set attendance to 100%
 # -------------------------------
+
 
 # Calculate data
 general_data = csv_file[general_headers]
@@ -72,11 +83,14 @@ project_data = project_data.rename('Total Proyectos')
 attendance_data = normalize_and_weight(csv_file, attendance_headers, percentages['attendance'])
 attendance_data = attendance_data.rename('Total Asistencia')
 
+portfolio_data = normalize_and_weight(csv_file, portfolio_headers, percentages['portfolio'])
+portfolio_data = portfolio_data.rename('Total Portafolio')
+
 final_data = normalize_and_weight(csv_file, final_headers, percentages['final'])
 final_data = final_data.rename('Total Final')
 
 # Calculate final grade
-final_grade = lab_data + parcial_data + project_data + attendance_data + final_data
+final_grade = lab_data + parcial_data + project_data + attendance_data + portfolio_data + final_data
 final_grade = final_grade.rename('Nota Final')
 
 # Ensure final grade doesn't exceed 100
@@ -88,7 +102,7 @@ final_grade_letter = final_grade_letter.rename('Nota Final en Letra')
 # -------------------------------
 
 # Merge data
-merged_data = pandas.concat([general_data, lab_data, parcial_data, project_data, attendance_data, final_data, final_grade, final_grade_letter], axis=1)
+merged_data = pandas.concat([general_data, lab_data, parcial_data, project_data, attendance_data, portfolio_data, final_data, final_grade, final_grade_letter], axis=1)
 
 # Order by last name
 merged_data = merged_data.sort_values(by='Last Name')
@@ -98,6 +112,11 @@ merged_data.to_csv('export.csv', index=False)
 
 # Export top3
 merged_data_copy = merged_data.copy()
-merged_data_copy['Nota Final'] = merged_data_copy['Nota Final'].astype(int).sort_values(ascending=False)
+# Sort by final grade
+merged_data_copy = merged_data_copy.sort_values(by='Nota Final', ascending=False)
 top3 = merged_data_copy.head(3)
 top3.to_csv('top3.csv', index=False)
+
+# Export only final grade with name and last name
+final_grade_only = merged_data[['First Name', 'Last Name', 'Nota Final', 'Nota Final en Letra']]
+final_grade_only.to_csv('final_grade_only.csv', index=False)
